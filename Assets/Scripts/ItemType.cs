ac5824b6a56f1ce5f346715da7791aef9b02672b
Salkogi07 <145998936+Salkogi07@users.noreply.github.com>
2024-08-16 16:03:49 +0900

    ss

--- Assets/Scripts/ItemType.cs
+++ Assets/Scripts/ItemType.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class ItemType : MonoBehaviour
 {
     public GameManager.InfoType type;
+    public AudioClip pickupSound; // 아이템을 먹을 때 재생할 소리
+    private AudioSource audioSource; // 오디오 소스를 저장할 변수
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.instance.UseItem((int)type);
-            Destroy(gameObject);
+
+            audioSource.PlayOneShot(pickupSound);
+
+            Destroy(gameObject, pickupSound.length);
         }
     }
 }
